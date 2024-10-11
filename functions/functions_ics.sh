b74ada90e846ce2e78103a4e0dfa0e120e1df87a
Keith Tweed <ktweed@ualberta.ca>
2024-10-10 20:59:07 -0600

    Edit dtoverlays for Pi-Repeater-8X

--- functions/functions_ics.sh
+++ functions/functions_ics.sh
@@ -19,15 +19,13 @@ function config_ics_controllers {
 			dtoverlay=fe-pi-audio
 			dtoverlay=i2s-mmap
 			
-			################################
-			#Enable mcp23s17 Overlay
-			#######################################
-			dtoverlay=mcp23017,addr=0x20,gpiopin=12
 			
-			#######################################
-			#Enable mcp3208 adc overlay
-			#######################################
-			dtoverlay=mcp3208:spi0-0-present,spi0-0-speed=1000000
+			##### MAIN BOARD #####
+			dtoverlay=mcp23017,addr=0x27,gpiopin=23
+			dtoverlay=mcp23017,addr=0x26,gpiopin=24
+			dtoverlay=mcp23017,addr=0x25,gpiopin=25
+			##### RELAY BOARD #####
+			dtoverlay=mcp23017,addr=0x24,gpiopin=13
 			DELIM
 			
 		echo "Complete"
@@ -43,72 +41,72 @@ function set_ics_asound {
 	    echo "------------------------------------"
 		###########################################
 
-		 cat >> "/etc/asound.conf" <<- DELIM
-				pcm.dmixed {
-				type dmix
-				ipc_key 1024
-				ipc_key_add_uid 0
-				slave.pcm "hw:0,0"
-			}
-			pcm.dsnooped {
-				type dsnoop
-				ipc_key 1025
-				slave.pcm "hw:0,0"
-			}
-			pcm.duplex {
-				type asym
-				playback.pcm "dmixed"
-				capture.pcm "dsnooped"
-			}
-			pcm.left {
-				type asym
-				playback.pcm "shared_left"
-				capture.pcm "dsnooped"
-			}
-			pcm.right {
-				type asym
-				playback.pcm "shared_right"
-				capture.pcm "dsnooped"
-			}
-			# Instruct ALSA to use pcm.duplex as the default device
-            pcm.!default {
-				type plug
-				slave.pcm "duplex"
-			}
-			ctl.!default {
-				type hw
-				card 0
-			}
-			# split left channel off
-			pcm.shared_left {
-				type plug
-				slave.pcm "hw:0"
-				slave.channels 2
-				ttable.0.0 1
-			}
-			# split right channel off
-			pcm.shared_right {
-				type plug
-				slave.pcm "hw:0"
-				slave.channels 2
-				ttable.1.1 1
-			}
-			#dtparam=i2s=on
-			Pcm_slave.hw_loopback {
-				Pcm "hw: loopback, 1.2"
-				Channels 2
-				Format RAW
-				Rate 16000
-			}
-			Pcm.plug_loopback {
-				Type plug
-				Slave hw_loopback
-				Ttable {
-					0.0 = 1
-					0.1 = 1
-				}
-			}
-			DELIM
+		#  cat >> "/etc/asound.conf" <<- DELIM
+		# 		pcm.dmixed {
+		# 		type dmix
+		# 		ipc_key 1024
+		# 		ipc_key_add_uid 0
+		# 		slave.pcm "hw:0,0"
+		# 	}
+		# 	pcm.dsnooped {
+		# 		type dsnoop
+		# 		ipc_key 1025
+		# 		slave.pcm "hw:0,0"
+		# 	}
+		# 	pcm.duplex {
+		# 		type asym
+		# 		playback.pcm "dmixed"
+		# 		capture.pcm "dsnooped"
+		# 	}
+		# 	pcm.left {
+		# 		type asym
+		# 		playback.pcm "shared_left"
+		# 		capture.pcm "dsnooped"
+		# 	}
+		# 	pcm.right {
+		# 		type asym
+		# 		playback.pcm "shared_right"
+		# 		capture.pcm "dsnooped"
+		# 	}
+		# 	# Instruct ALSA to use pcm.duplex as the default device
+        #     pcm.!default {
+		# 		type plug
+		# 		slave.pcm "duplex"
+		# 	}
+		# 	ctl.!default {
+		# 		type hw
+		# 		card 0
+		# 	}
+		# 	# split left channel off
+		# 	pcm.shared_left {
+		# 		type plug
+		# 		slave.pcm "hw:0"
+		# 		slave.channels 2
+		# 		ttable.0.0 1
+		# 	}
+		# 	# split right channel off
+		# 	pcm.shared_right {
+		# 		type plug
+		# 		slave.pcm "hw:0"
+		# 		slave.channels 2
+		# 		ttable.1.1 1
+		# 	}
+		# 	#dtparam=i2s=on
+		# 	Pcm_slave.hw_loopback {
+		# 		Pcm "hw: loopback, 1.2"
+		# 		Channels 2
+		# 		Format RAW
+		# 		Rate 16000
+		# 	}
+		# 	Pcm.plug_loopback {
+		# 		Type plug
+		# 		Slave hw_loopback
+		# 		Ttable {
+		# 			0.0 = 1
+		# 			0.1 = 1
+		# 		}
+		# 	}
+		# 	DELIM
 			
 		echo "Complete"
 fi
